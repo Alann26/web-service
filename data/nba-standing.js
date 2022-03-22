d466c9ee3db19a4edd8c0cf3c75e335bfccada2a
Alan Nguyen <Alannguyen26@yahoo.com>
2022-03-22 02:51:13 +0000

    does assignment

---
+++ data/nba-standing.js
@@ -0,0 +1,95 @@
+{
+	"title": "Basketball Teams by Standings",
+	"teams":[
+			  {
+				"Rank":1,
+				"Team":"Phoenix Suns",
+				"State":"Arizona",
+				"Coach":"Monty Williams",
+				"Mascot":"The Suns Gorilla",
+				"Image":"sun.jpg"
+			  },
+
+    			  {
+				"Rank":1,
+				"Team":"Miami Heat",
+				"State":"Florida",
+				"Coach":"Erik Spoelstra",
+				"Mascot":"Burnie",
+				"Image":"heat.jpg"
+			  },
+
+    			  {
+				"Rank":2,
+				"Team":"Memphis Grizzlies",
+				"State":"Tennessee",
+				"Coach":"Taylor Jenkins",
+				"Mascot":"Grizz",
+				"Image":"memphis.jpg"
+			  },
+
+    			  {
+				"Rank":2,
+				"Team":"Milwaukee Bucks",
+				"State":"Wisconsin",
+				"Coach":"Mike Budenholzer",
+				"Mascot":"Bango the Buck",
+				"Image":"bucks.jpg"
+			  },
+
+    			  {
+				"Rank":3,
+				"Team":"Golden State Warriors",
+				"State":"California",
+				"Coach":"Steve Kerr",
+				"Mascot":"None",
+				"Image":"warriors.jpg"
+			  },
+
+    			  {
+				"Rank":3,
+				"Team":"Philadelphia 76ers",
+				"State":"Pennsylvania",
+				"Coach":"Doc Rivers",
+				"Mascot":"Franklin the Dog",
+				"Image":"76ers.jpg"
+			  },
+
+    			  {
+				"Rank":4,
+				"Team":"Utah Jazz",
+				"State":"Utah",
+				"Coach":"Quin Snyder",
+				"Mascot":"Jazz Bear",
+				"Image":"jazz.jpg"
+			  },
+
+    			  {
+				"Rank":4,
+				"Team":"Chicago Bulls",
+				"State":"Illinois",
+				"Coach":"Billy Donovan",
+				"Mascot":"Benny the Bull",
+				"Image":"bulls.jpg"
+			  },
+
+    			  {
+				"Rank":5,
+				"Team":"Dallas Mavericks",
+				"State":"Texas",
+				"Coach":"Jason Kidd",
+				"Mascot":"Champ, Mavs Man",
+				"Image":"mavs.jpg"
+			  },
+
+    			  {
+				"Rank":5,
+				"Team":"Boston Celtics",
+				"State":"Massachusetts",
+				"Coach":"Ime Udoka",
+				"Mascot":"Lucky the Leprechaun",
+				"Image":"celtics.jpg"
+    
+			  }
+			]
+}
--- index.php
+++ index.php
@@ -1,23 +1,86 @@
 <html>
-    <head>
-        <title>PHP File!</title>
-    </head>
-<body>
-    <h1>PHP File!</h1>
+<head>
+<title>Bond Web Service Demo</title>
+<style>
+body {font-family:georgia;}
+  .nba{
+    border:1px solid #E77DC2;
+    border-radius: 5px;
+    padding: 5px;
+    margin-bottom:5px;
+    position:relative;
+  }
+ 
+  .pic{
+    position:absolute;
+    right:10px;
+    top:10px;
+  }
 
-    <p>This is HTML!</p>
+  .pic img{
+    max-width:70px;
+  }
 
-    <?php
+</style>
+<script src="https://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
 
-    echo "<p>This is PHP!</p>";
+<script type="text/javascript">
+function nbaTemplate(team){
+   return `<div class="nba">
+      <b>Rank: </b> ${team.Rank}<br />
+      <b>Team: </b> ${team.Team}<br />
+      <b>State: </b> ${team.State}<br />
+      <b>Coach: </b> ${team.Coach}<br />
+      <b>Mascot: </b> ${team.Mascot}<br />
+      <div class="pic"><img src="thumbnails/${team.Image}" /></div>
+    </div>`;
+}
+$(document).ready(function() {
+    $('.category').click(function(e){
+    e.preventDefault(); //stop default action of the link
+        cat = $(this).attr("href");  //get category from URL
+    var request = $.ajax({
+      url: "api.php?cat=" + cat,
+      method: "GET",
+      dataType: "json"
+    });
+    request.done(function( data ) {
+      console.log(data);
+      $("#nbatitle").html(data.title);
+      //clears the previous films
+      $("#nbas").html("");
+      //loops through films 
+      $.each(data.teams,function(key,value){
+      let str = nbaTemplate(value);
+      $("<div></div>").html(str).appendTo("#nbas");
 
-    ?>
+      });
+      //Place the title of the web service on the page
+      //$("#output").text(JSON.stringify(data));
 
-    <p>This is HTML!</p>
-
-    <p>Hello from repl.it!</p>
-</body>
-
+      // let myData = JSON.stringify(data,null,4);
+      // myData = "<pre>" + myData + "</pre>";
+      // $("#output").html(myData);
 
 
+    });
+    request.fail(function(xhr, status, error) {
+      //Ajax request failed.
+      var errorMessage = xhr.status + ': ' + xhr.statusText
+      alert('Error - ' + errorMessage);
+    });
+    });
+});
+</script>
+</head>
+    <body>
+    <h1>National Basketball Association</h1>
+        <a href="conference" class="category">NBA Conference</a><br />
+        <a href="standing" class="category">NBA Teams</a>
+        <h3 id="nbatitle">Title Will Go Here</h3>
+        <div id="nbas">
+            <p>NBA</p>
+        </div>
+        <div id="output">Results go here</div>
+    </body>
 </html>
